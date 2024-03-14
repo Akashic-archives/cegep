b5ce0376d8dae53034a332cd389503da4e8791cf
momo <mhamedbattioui@gmail.com>
2024-03-14 16:09:43 -0400

    gauss

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cegep
+
+Quelques projets que j'ai fait au cegep.
+
+Projet 1:
+Resoudre un system a plusieurs equations plusieurs inconnues a l'aide d'un programme.
+
+
---
+++ gauss-jordan/main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    int i,j;
+    System.out.println("Entrez la taille de la matrice:");
+    System.out.println("i:");
+    i = sc.nextInt();
+    System.out.println("j:");
+    j = sc.nextInt();
+
+    Scanner sc = new Scanner(System.in);
+    int [][] tab = new tab[i][j]; 
+  }
+}
